f79fa464f185d959446c1c20670da4a253d064b6
Bryan Bai <byebyebryan@gmail.com>
2022-03-26 21:08:49 -0400

    add bryan's config for gmmk pro and kbd67

--- keyboards/gmmk/pro/ansi/keymaps/bryan/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/bryan/config.h
@@ -1,25 +1,36 @@
+/* Copyright 2021 stickandgum
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
-#include "config_common.h"
-#define VENDOR_ID       0x4B42 // KB
-#define PRODUCT_ID      0x1225
-#define DEVICE_VER      0x0002
-#define MANUFACTURER    KBDfans
-#define PRODUCT         kbd67mkiirgb v2
 
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-#define MATRIX_ROW_PINS {F0, F1, F4, E6, C6 }
-#define MATRIX_COL_PINS {F7, F6, F5, C7, B0, B1, B2, B3, B4, D7, D6, D4, D5, D3, D2}
-#define DIODE_DIRECTION COL2ROW
+// I like NKRO.. but let's not force it. @drashna pointed out some older hardware has issues - Toggle it via keycode if you like.
+// #define FORCE_NKRO
+
+// At the time of this, there are 41 effects! That may be a bit too many to cycle through - keeping what I believe is the best.
 #ifdef RGB_MATRIX_ENABLE
-#define RGB_MATRIX_LED_PROCESS_LIMIT 4
-#define RGB_MATRIX_LED_FLUSH_LIMIT 26
-#define DEBOUNCE 3
-#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#define RGB_MATRIX_KEYPRESSES
 
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+// #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+
+// Below added per: https://beta.docs.qmk.fm/using-qmk/hardware-features/lighting/feature_rgb_matrix#suspended-state-id-suspended-state
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+    // #define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Heatmap, Rain
+    #define RGB_MATRIX_KEYPRESSES // REACTIVE, SPLASH modes
+
+// Soon we need to ENABLE modes by DEFAULT soon as per: https://github.com/qmk/qmk_firmware/pull/13758 Thanks @noroadsleft
     //  #define DISABLE_RGB_MATRIX_SOLID_COLOR               // BUILT IN (MAPPED!) Static single color.
      #define DISABLE_RGB_MATRIX_ALPHAS_MODS               // Accenting various keys on the keyboard.
      #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN          // Top down rainbow!
@@ -60,13 +71,5 @@
     //  #define DISABLE_RGB_MATRIX_SPLASH                    // Full rainbow pulses from key hit. All else black.
      #define DISABLE_RGB_MATRIX_MULTISPLASH               // Full rainbow pulses from multi-keys. All else black.
     //  #define DISABLE_RGB_MATRIX_SOLID_SPLASH              // Single color pulses from key hit. All else black. (Using solid_reactive_wide instead).
-     #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
-     
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
-#    define DRIVER_ADDR_1 0b1110100
-#    define DRIVER_ADDR_2 0b1110111
-#    define DRIVER_COUNT 2
-#    define DRIVER_1_LED_TOTAL 35
-#    define DRIVER_2_LED_TOTAL 32
-#    define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+     #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH         // Single color pulses from muli-keys. All else black. (Using solid_reactive_wide instead).
 #endif
